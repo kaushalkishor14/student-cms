b8a8e1347d644a029e59d7a9e8be98fe2a9dc4dd
Suraj Kumar <surajsinhaking77@gmail.com>
2024-06-05 01:28:04 +0530

    done here

---
+++ postinstall.js
@@ -0,0 +1,14 @@
+const os = require('os');
+const { execSync } = require('child_process');
+
+const platform = os.platform();
+
+if (platform === 'linux') {
+  try {
+    execSync('npm install @rollup/rollup-linux-x64-gnu', { stdio: 'inherit' });
+  } catch (error) {
+    console.error('Error installing @rollup/rollup-linux-x64-gnu:', error);
+  }
+} else {
+  console.log(`Skipping @rollup/rollup-linux-x64-gnu installation on ${platform} platform.`);
+}
